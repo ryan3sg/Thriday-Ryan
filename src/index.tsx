0f3766fbbae1c3f4b6714074d5ad940af57f059d
Rye Ayan <ryan3sg@localhost>
2025-03-29 14:26:34 +0800

    chore: add redux boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './ErrorBoundary';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <Provider store={store}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </Provider>
     </React.StrictMode>,
   );
 }
